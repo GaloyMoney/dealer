8c2d0c1d00f90eef9164e7ab04c92674dbf3ae1e
krtk6160 <krtk.6160@gmail.com>
2020-10-05 15:36:22 +0530

    fix ci

--- src/App.js
+++ src/App.js
@@ -14,18 +14,17 @@ function App() {
       <Container>
         <br />
         <Row>
-          <Col >
-            <h2>Connect to the Bitcoin Beach <br /> Lightning Node</h2>
-          </Col>
-          <Col xs={9}>
+          <Col>
             <Jumbotron>
               <Container>
                 <Row>
                   <Col>
-                    <label>Node Public Key: </label> <p style={{fontSize: 'small'}}>0325bb9bda523a85dc834b190289b7e25e8d92615ab2f2abffbe97983f0bb12ffb</p>
-                    <label> Node Connection String:</label> <p style={{fontSize: 'small'}}>0325bb9bda523a85dc834b190289b7e25e8d92615ab2f2abffbe97983f0bb12ffb<br/>@35.188.203.196:9735</p>
-                    <hr/>
-                    </Col>
+                    <h2>Connect to the Bitcoin Beach Lightning Node</h2>
+                    <br/>
+                    <label>Node Public Key: </label> <p style={{ fontSize: 'small' }}>0325bb9bda523a85dc834b190289b7e25e8d92615ab2f2abffbe97983f0bb12ffb</p>
+                    <label> Node Connection String:</label> <p style={{ fontSize: 'small' }}>0325bb9bda523a85dc834b190289b7e25e8d92615ab2f2abffbe97983f0bb12ffb@35.188.203.196:9735</p>
+                    <hr />
+                  </Col>
                   <Col xs={3}>
                     <Image src={"./qrcode.png"} fluid />
                   </Col>
