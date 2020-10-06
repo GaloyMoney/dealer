5968f39ab20853391d025db780143bc211780887
krtk6160 <krtk.6160@gmail.com>
2020-10-07 02:30:48 +0530

    use env var for pubkey and addr

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,11 @@ RUN yarn install --prod --frozen-lockfile
 
 COPY ./app ./
 
-RUN yarn build
+#lnd node params
+ARG ADDR=35.188.203.196:9735
+ARG PUBKEY=0325bb9bda523a85dc834b190289b7e25e8d92615ab2f2abffbe97983f0bb12ffb
+
+RUN REACT_APP_PUB_KEY=$PUBKEY REACT_APP_ADDR=$ADDR yarn build
 
 FROM nginx:alpine
 
--- app/src/App.js
+++ app/src/App.js
@@ -21,8 +21,8 @@ function App() {
                   <Col>
                     <h2>Connect to the Bitcoin Beach Lightning Node</h2>
                     <br/>
-                    <label>Node Public Key: </label> <p style={{ fontSize: 'small' }}>0325bb9bda523a85dc834b190289b7e25e8d92615ab2f2abffbe97983f0bb12ffb</p>
-                    <label> Node Connection String:</label> <p style={{ fontSize: 'small' }}>0325bb9bda523a85dc834b190289b7e25e8d92615ab2f2abffbe97983f0bb12ffb@35.188.203.196:9735</p>
+                    <label>Node Public Key: </label> <p style={{ fontSize: 'small' }}>{process.env.REACT_APP_PUB_KEY}</p>
+                    <label> Node Connection String:</label> <p style={{ fontSize: 'small' }}>{process.env.REACT_APP_PUB_KEY}@{process.env.REACT_APP_ADDR}</p>
                     <hr />
                   </Col>
                   <Col xs={3}>
