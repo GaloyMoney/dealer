9b95796059acc4296f446a364cb4f32563928310
krtk6160 <krtk.6160@gmail.com>
2020-10-08 20:22:49 +0530

    use bitcoinbeach logo

--- app/src/components/header.js
+++ app/src/components/header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
+import Image from 'react-bootstrap/Image'
 
 function Header() {
 	return (
-		<Navbar bg="dark" variant="dark">
+		<Navbar bg="dark">
 		<Container>
-			<Navbar.Brand href="https://try.galoy.io">Galoy</Navbar.Brand>
+			<Navbar.Brand href="https://bitcoinbeach.com"><Image src="BBLogo.png" rounded/> </Navbar.Brand>
 		</Container>
 		</Navbar>
 	)
