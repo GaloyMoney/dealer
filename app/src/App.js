44b31ded4674b389b4cd5840c9badc7ca2d4e301
krtk6160 <krtk.6160@gmail.com>
2020-11-26 18:02:38 +0530

    try fix for desktop

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/home.js'
+import Header from './components/header.js'
 import Receive from './components/receive.js'
 import { useRoutes } from 'hookrouter';
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const Redirect = () => {
   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
@@ -12,13 +17,34 @@ const Redirect = () => {
   }
 
   if (/android/i.test(userAgent)) {
-    window.location.replace("https://play.google.com/store/apps/details?id=com.galoyapp")
+    return window.location.replace("https://play.google.com/store/apps/details?id=com.galoyapp")
   }
 
   // iOS detection from: http://stackoverflow.com/a/9039885/177710
   if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    window.location.replace("itms-apps://itunes.apple.com/app/bitcoin-beach-wallet/id1531383905")
+    return window.location.replace("itms-apps://itunes.apple.com/app/bitcoin-beach-wallet/id1531383905")
   }
+  return (
+    <div>
+    <Header />
+      <Container>
+      <br />
+      <br />
+        <Row>
+          <Col>
+            <a href="https://play.google.com/store/apps/details?id=com.galoyapp">
+            <Image src={process.env.PUBLIC_URL + '/google-play-badge.png'} rounded/>
+            </a>
+          </Col>
+          <Col>
+            <a href="https://apps.apple.com/app/bitcoin-beach-wallet/id1531383905">
+            <Image src={process.env.PUBLIC_URL + '/apple-app-store.png'} rounded/>
+            </a>
+          </Col>
+      </Row>
+      </Container>
+    </div>
+  )
 }
 
 const routes = {
