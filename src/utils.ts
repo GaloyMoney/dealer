9684b11001402551d3d4bb0ee908ee6bcb4e19a7
Sebastien Verreault <sebver@pm.me>
2021-07-30 07:13:02 +0000

    Add sats to btc conversion

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,11 @@
 export async function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
+
+export const btc2sat = (btc: number) => {
+  return Math.round(btc * Math.pow(10, 8))
+}
+
+export const sat2btc = (sat: number) => {
+  return sat / Math.pow(10, 8)
+}
