cc3a4a70315c33bcfbe02535eaf3a286c5a7eb15
Sebastien Verreault <sebver@pm.me>
2021-07-31 04:38:29 +0000

    Add entrypoint code for new dealer structure

--- src/entrypoint/dealer.ts
+++ src/entrypoint/dealer.ts
@@ -1,28 +1,20 @@
-// import { setupMongoConnection } from "../mongodb"
-// import { baseLogger } from "../logger"
-// import { User } from "../schema"
-// import { getCurrentPrice } from "../realtimePrice"
-// import { UserWallet } from "../userWallet"
-// import { Dealer } from "../dealer/Dealer"
+import { baseLogger } from "../logger"
+import { Dealer } from "../Dealer"
 
-// const main = async () => {
-//   const lastPrice = await getCurrentPrice()
-//   UserWallet.setCurrentPrice(lastPrice)
+const main = async () => {
+  const logger = baseLogger.child({ module: "cron" })
+  const dealer = new Dealer(logger)
 
-//   const logger = baseLogger.child({ module: "cron" })
-//   const dealerUser = await User.findOne({ role: "dealer" })
-//   const dealer = new Dealer({ user: dealerUser, logger })
+  await dealer.updatePositionAndLeverage()
 
-//   await dealer.updatePositionAndLeverage()
+  // FIXME: we need to exit because we may have some pending promise
+  process.exit(0)
+}
 
-//   // FIXME: we need to exit because we may have some pending promise
-//   process.exit(0)
-// }
-
-// if (require.main === module) {
-//   try {
-//     main()
-//   } catch (error) {
-//     baseLogger.warn(`Error in Dealer job: ${error}`)
-//   }
-// }
+if (require.main === module) {
+  try {
+    main()
+  } catch (error) {
+    baseLogger.warn(`Error in Dealer job: ${error}`)
+  }
+}
