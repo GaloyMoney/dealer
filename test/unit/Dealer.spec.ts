43feee6af75919a9dc8bcc9b012c3b7e3981d6be
Sebastien Verreault <sebver@pm.me>
2021-08-08 15:54:46 +0000

    Add initial dealer test

---
+++ test/unit/Dealer.spec.ts
@@ -0,0 +1,101 @@
+import { baseLogger } from "src/logger"
+import { Dealer } from "src/Dealer"
+import { OrderStatus } from "src/ExchangeTradingType"
+import { SupportedExchange } from "src/ExchangeConfiguration"
+import { OkexExchangeMockBuilder } from "../mocks/OkexExchangeMockBuilder"
+
+beforeAll(async () => {
+  // Init exchange secrets
+  for (const exchangeId in SupportedExchange) {
+    process.env[`${exchangeId.toUpperCase()}_KEY`] = exchangeId
+    process.env[`${exchangeId.toUpperCase()}_SECRET`] = exchangeId
+    process.env[`${exchangeId.toUpperCase()}_PASSWORD`] = exchangeId
+  }
+})
+
+class MockScenarioBuilder {
+  mockBuilder
+  constructor() {
+    this.mockBuilder = new OkexExchangeMockBuilder()
+    this.mockBuilder.mockThis(
+      10000,
+      0,
+      0,
+      0,
+      0,
+      false,
+      false,
+      false,
+      NaN,
+      OrderStatus.Canceled,
+      0,
+      OrderStatus.Canceled,
+      false,
+      false,
+      false,
+      true,
+      true,
+    )
+  }
+
+  public getExchangeMock() {
+    return this.mockBuilder.getExchangeMockObject()
+  }
+
+  public getWalletMock() {
+    return this.mockBuilder.getWalletMockObject()
+  }
+}
+
+const mockScenarios = new MockScenarioBuilder()
+
+jest.mock("ccxt", () => ({
+  okex5: function () {
+    const exchangeMock = mockScenarios.getExchangeMock()
+    return exchangeMock
+  },
+}))
+
+jest.mock("src/DealerMockWallet", () => ({
+  DealerMockWallet: function () {
+    const walletMock = mockScenarios.getWalletMock()
+    return walletMock
+  },
+}))
+
+describe("Dealer", () => {
+  describe("wallet mocks", () => {
+    it("should run clear", async () => {
+      const logger = baseLogger.child({ module: "cron" })
+      const dealer = new Dealer(logger)
+
+      //   const r0 = await dealer.getWalletOnChainAddress()
+      //   console.log(JSON.stringify(r0))
+
+      //   const r1 = await dealer.getUsdLiability()
+      //   console.log(JSON.stringify(r1))
+
+      //   const r2 = await dealer.onChainPay({ address: "", btcAmountInSats: 1, memo: "" })
+      //   console.log(JSON.stringify(r2))
+    })
+  })
+  describe.only("first call", () => {
+    it("should run return an order", async () => {
+      const logger = baseLogger.child({ module: "cron" })
+      const dealer = new Dealer(logger)
+
+      //   const r0 = await dealer.getWalletOnChainAddress()
+      //   console.log(JSON.stringify(r0))
+
+      //   const r1 = await dealer.getUsdLiability()
+      //   console.log(JSON.stringify(r1))
+
+      //   const r2 = await dealer.onChainPay({ address: "", btcAmountInSats: 1, memo: "" })
+      //   console.log(JSON.stringify(r2))
+
+      const result = await dealer.updatePositionAndLeverage()
+      console.log(JSON.stringify(result))
+      expect(result.ok).toBeTruthy()
+    })
+  })
+})
