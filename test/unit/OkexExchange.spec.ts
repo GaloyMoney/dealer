d2349bad6789df5976a83b94461d1f1920aeb0df
Sebastien Verreault <sebver@pm.me>
2021-08-13 12:13:53 +0000

    Update okex exchange specific tests

--- test/unit/OkexExchange.spec.ts
+++ test/unit/OkexExchange.spec.ts
@@ -23,7 +23,66 @@ beforeAll(async () => {
   }
 })
 
+function getValidFetchPositionResponse(args: {
+  last: number
+  notionalUsd: number
+  margin: number
+}) {
+  return {
+    last: `${args.last}`,
+    notionalUsd: `${args.notionalUsd}`,
+    margin: `${args.margin}`,
+  }
+}
+
+function getValidFetchBalanceResponse(args: { balance: number }) {
+  return {
+    info: {
+      data: [
+        {
+          totalEq: `${args.balance}`,
+        },
+      ],
+    },
+  }
+}
+
+function getValidPublicGetPublicInstrumentsResponse(args: {
+  contractValue: number
+  contractMinimumSize: number
+  contractValueCurrency: TradeCurrency
+  instrumentType: string
+  instrumentId: string
+}) {
+  return {
+    code: "0",
+    data: [
+      {
+        ctVal: `${args.contractValue}`,
+        minSz: `${args.contractMinimumSize}`,
+        ctValCcy: `${args.contractValueCurrency}`,
+        instId: `${args.instrumentId}`,
+        instType: `${args.instrumentType}`,
+      },
+    ],
+    msg: "",
+  }
+}
+
 const falsyArgs = [null, undefined, NaN, 0, "", false]
+
+const exchangeMock = {
+  checkRequiredCredentials: jest.fn().mockReturnValue(true),
+  fetchPosition: jest.fn(),
+  fetchBalance: jest.fn(),
+  publicGetPublicInstruments: jest.fn(),
+}
+
+jest.mock("ccxt", () => ({
+  okex5: function () {
+    return exchangeMock
+  },
+}))
 
 describe("OkexExchange", () => {
   describe("constructor", () => {
@@ -74,38 +133,164 @@ describe("OkexExchange", () => {
       }
     })
 
-    it("should return proper leverageRatio", async () => {
+    it("should return proper risk numbers", async () => {
       const exchangeConfig = new OkexExchangeConfiguration()
       const exchange = new OkexExchange(exchangeConfig, baseLogger)
-      // need a mock up of ExchangeBase.fetchPosition()
-      //  and of ExchangeBase.fetchBalance()
-      // to test the logic in this.getAccountAndPositionRisk()
-      // TODO:
-      //  test leverageRatio result from mock'd ExchangeBase.privateGetAccount() response
-      expect(exchange).toBeInstanceOf(OkexExchange)
-    })
 
-    it("should return proper collateralInUsd", async () => {
-      const exchangeConfig = new OkexExchangeConfiguration()
-      const exchange = new OkexExchange(exchangeConfig, baseLogger)
-      // need a mock up of ExchangeBase.fetchPosition()
-      //  and of ExchangeBase.fetchBalance()
-      // to test the logic in this.getAccountAndPositionRisk()
-      // TODO:
-      //  test collateralInUsd result from mock'd ExchangeBase.privateGetAccount() response
-      expect(exchange).toBeInstanceOf(OkexExchange)
+      const expectedLast = 10000
+      const expectedMargin = 10 / expectedLast
+      const expectedNotional = 100
+      const expectedLeverageRatio = expectedNotional / expectedLast / expectedMargin
+      const expectedCollateralInUsd = expectedMargin * expectedLast
+      const expectedExposureInUsd = expectedNotional
+
+      exchangeMock.fetchPosition.mockImplementationOnce(() => {
+        return getValidFetchPositionResponse({
+          last: expectedLast,
+          margin: expectedMargin,
+          notionalUsd: expectedNotional,
+        })
+      })
+
+      const expectedTotalEquity = 1234
+      exchangeMock.fetchBalance.mockImplementationOnce(() => {
+        return getValidFetchBalanceResponse({ balance: expectedTotalEquity })
+      })
+
+      const result = await exchange.getAccountAndPositionRisk(expectedLast)
+      expect(result.ok).toBeTruthy()
+      if (result.ok) {
+        const risk = result.value
+        expect(risk.lastBtcPriceInUsd).toBe(expectedLast)
+        expect(risk.leverageRatio).toBe(expectedLeverageRatio)
+        expect(risk.collateralInUsd).toBe(expectedCollateralInUsd)
+        expect(risk.exposureInUsd).toBe(expectedExposureInUsd)
+        expect(risk.totalAccountValueInUsd).toBe(expectedTotalEquity)
+      }
     })
   })
 
   describe("getInstrumentDetails", () => {
-    it("should return proper collateralInUsd", async () => {
+    it("should return error when exchange api call response is falsy", async () => {
       const exchangeConfig = new OkexExchangeConfiguration()
       const exchange = new OkexExchange(exchangeConfig, baseLogger)
-      // need a mock up of ccxt exchange.publicGetPublicInstruments()
-      // to test the logic in this.getInstrumentDetails()
-      // TODO:
-      //  test collateralInUsd result from mock'd ExchangeBase.privateGetAccount() response
-      expect(exchange).toBeInstanceOf(OkexExchange)
+      for (const arg of falsyArgs) {
+        // Push the exchange api required mocked calls
+        exchangeMock.publicGetPublicInstruments.mockImplementationOnce(() => {
+          return arg
+        })
+        // Test based on above mock setup
+        const result = await exchange.getInstrumentDetails()
+        expect(result.ok).toBeFalsy()
+        if (!result.ok) {
+          expect(result.error.message).toEqual(ApiError.UNSUPPORTED_API_RESPONSE)
+        }
+      }
+    })
+
+    it("should return error when contract currency is not USD", async () => {
+      const exchangeConfig = new OkexExchangeConfiguration()
+      const exchange = new OkexExchange(exchangeConfig, baseLogger)
+      // Push the exchange api required mocked calls
+      exchangeMock.publicGetPublicInstruments.mockImplementationOnce(
+        ({ instrumentType, instrumentId }) => {
+          return getValidPublicGetPublicInstrumentsResponse({
+            contractValue: 1,
+            contractMinimumSize: 1,
+            contractValueCurrency: TradeCurrency.BTC,
+            instrumentType,
+            instrumentId,
+          })
+        },
+      )
+      // Test based on above mock setup
+      const result = await exchange.getInstrumentDetails()
+      expect(result.ok).toBeFalsy()
+      if (!result.ok) {
+        expect(result.error.message).toEqual(ApiError.UNSUPPORTED_CURRENCY)
+      }
+    })
+
+    it("should return error when contract minimum size is not positive", async () => {
+      const exchangeConfig = new OkexExchangeConfiguration()
+      const exchange = new OkexExchange(exchangeConfig, baseLogger)
+
+      const invalidQuantity = [0, -1]
+      for (const quantity of invalidQuantity) {
+        // Push the exchange api required mocked calls
+        exchangeMock.publicGetPublicInstruments.mockImplementationOnce(
+          ({ instrumentType, instrumentId }) => {
+            return getValidPublicGetPublicInstrumentsResponse({
+              contractValue: 1,
+              contractMinimumSize: quantity,
+              contractValueCurrency: TradeCurrency.USD,
+              instrumentType,
+              instrumentId,
+            })
+          },
+        )
+        // Test based on above mock setup
+        const result = await exchange.getInstrumentDetails()
+        expect(result.ok).toBeFalsy()
+        if (!result.ok) {
+          expect(result.error.message).toEqual(ApiError.NON_POSITIVE_QUANTITY)
+        }
+      }
+    })
+
+    it("should return error when contract face value is not positive", async () => {
+      const exchangeConfig = new OkexExchangeConfiguration()
+      const exchange = new OkexExchange(exchangeConfig, baseLogger)
+
+      const invalidQuantity = [0, -1]
+      for (const quantity of invalidQuantity) {
+        // Push the exchange api required mocked calls
+        exchangeMock.publicGetPublicInstruments.mockImplementationOnce(
+          ({ instrumentType, instrumentId }) => {
+            return getValidPublicGetPublicInstrumentsResponse({
+              contractValue: quantity,
+              contractMinimumSize: 1,
+              contractValueCurrency: TradeCurrency.USD,
+              instrumentType,
+              instrumentId,
+            })
+          },
+        )
+        // Test based on above mock setup
+        const result = await exchange.getInstrumentDetails()
+        expect(result.ok).toBeFalsy()
+        if (!result.ok) {
+          expect(result.error.message).toEqual(ApiError.NON_POSITIVE_PRICE)
+        }
+      }
+    })
+
+    it("should return proper contract details", async () => {
+      const exchangeConfig = new OkexExchangeConfiguration()
+      const exchange = new OkexExchange(exchangeConfig, baseLogger)
+
+      // Push the exchange api required mocked calls
+      const contractValue = 1
+      const contractMinimumSize = 1
+      exchangeMock.publicGetPublicInstruments.mockImplementationOnce(
+        ({ instrumentType, instrumentId }) => {
+          return getValidPublicGetPublicInstrumentsResponse({
+            contractValue: contractValue,
+            contractMinimumSize: contractMinimumSize,
+            contractValueCurrency: TradeCurrency.USD,
+            instrumentType,
+            instrumentId,
+          })
+        },
+      )
+      // Test based on above mock setup
+      const result = await exchange.getInstrumentDetails()
+      expect(result.ok).toBeTruthy()
+      if (result.ok) {
+        const details = result.value
+        expect(details.contractFaceValue).toBe(contractValue)
+        expect(details.minimumOrderSizeInContract).toBe(contractMinimumSize)
+      }
     })
   })
 })
