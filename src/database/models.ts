e86ff42c21d4480bb2bcad28277ec98af62a2dc7
Sebastien Verreault <sebver@pm.me>
2021-09-29 07:29:21 +0000

    Add graphql related models

--- src/database/models.ts
+++ src/database/models.ts
@@ -8,3 +8,42 @@ export interface InFlightTransfer {
   updatedTimestamp?: string
   createdTimestamp?: string
 }
+
+export interface WalletObject {
+  id: string
+  balance: {
+    currency: string
+    amount: number
+  }
+}
+
+export interface Wallet {
+  id?: number
+  jsonData: WalletObject[]
+  updatedTimestamp?: string
+  createdTimestamp?: string
+}
+
+export interface LastOnChainAddressObject {
+  id: string
+}
+
+export interface LastOnChainAddress {
+  id?: number
+  jsonData: LastOnChainAddressObject
+  updatedTimestamp?: string
+  createdTimestamp?: string
+}
+
+export interface OnChainPayObject {
+  address: string
+  amount: number
+  memo: string
+}
+
+export interface OnChainPay {
+  id?: number
+  jsonData: OnChainPayObject
+  updatedTimestamp?: string
+  createdTimestamp?: string
+}
