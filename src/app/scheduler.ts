39218bd60f6d8915625a691dd693a1b2b6070309
Sebastien Verreault <sebver@pm.me>
2021-10-27 11:27:11 +0000

    Merge dealer and exporter together

--- src/app/scheduler.ts
+++ src/app/scheduler.ts
@@ -1,15 +1,23 @@
 import { baseLogger } from "../services/logger"
 import cron from "node-cron"
 import { Dealer } from "../Dealer"
+import { exporter } from "../servers/exporter/exporter"
 
 const logger = baseLogger.child({ module: "cron" })
 
-cron.schedule("0,10,20,30,40,50 * * * *", async () => {
-  const dealer = new Dealer(logger)
-  try {
-    logger.info("Starting dealer")
-    await dealer.updatePositionAndLeverage()
-  } catch (error) {
-    baseLogger.warn(`Error in Dealer job: ${error}`)
-  }
-})
+const scheduler = async () => {
+  cron.schedule("0,10,20,30,40,50 * * * *", async () => {
+    const dealer = new Dealer(logger)
+    try {
+      logger.info("Starting dealer")
+      await dealer.updatePositionAndLeverage()
+    } catch (error) {
+      baseLogger.warn(`Error in Dealer job: ${error}`)
+    }
+  })
+}
+
+Promise.all([
+  exporter().catch((err) => logger.error(err)),
+  scheduler().catch((err) => logger.error(err)),
+])
