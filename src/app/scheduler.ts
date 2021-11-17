ce7edc21eec2009ea2a4ad18833acf1f486113ca
Sebastien Verreault <sebver@pm.me>
2021-11-17 07:58:40 +0000

    Add programmatic db migration at start of dealer

--- src/app/scheduler.ts
+++ src/app/scheduler.ts
@@ -2,8 +2,20 @@ import { baseLogger } from "../services/logger"
 import cron from "node-cron"
 import { Dealer } from "../Dealer"
 import { exporter } from "../servers/exporter/exporter"
+import nodePgMigrate from "node-pg-migrate"
+import { RunnerOption } from "node-pg-migrate/dist/types"
 
 const logger = baseLogger.child({ module: "cron" })
+
+const options = {
+  dir: "migrations",
+  createSchema: true,
+  schema: "dealer",
+  direction: "up",
+  count: 100,
+  verbose: true,
+  databaseUrl: process.env["DATABASE_URL"],
+} as RunnerOption
 
 const scheduler = async () => {
   cron.schedule("0,10,20,30,40,50 * * * *", async () => {
@@ -20,4 +32,5 @@ const scheduler = async () => {
 Promise.all([
   exporter().catch((err) => logger.error(err)),
   scheduler().catch((err) => logger.error(err)),
+  nodePgMigrate(options).catch((err) => logger.error(err)),
 ])
