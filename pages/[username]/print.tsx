5bcfc6129e3e1e88fce96ec95f241f12a54920d5
fiatjaf <fiatjaf@gmail.com>
2021-11-26 19:44:00 -0300

    Show lnurl QR code on /print page.

--- pages/[username]/print.tsx
+++ pages/[username]/print.tsx
@@ -1,17 +1,40 @@
-import { useRouter } from "next/router"
-import { useRef } from "react"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Card from "react-bootstrap/Card"
-import Container from "react-bootstrap/Container"
 import Button from "react-bootstrap/Button"
+import Container from "react-bootstrap/Container"
+import originalUrl from "original-url"
 import ReactToPrint from "react-to-print"
+import { bech32 } from "bech32"
 import { QRCode } from "react-qrcode-logo"
+import { useRouter } from "next/router"
+import { useRef, useState } from "react"
 
-const PrintQR = () => {
+export async function getServerSideProps({ req, params: { username } }) {
+  const url = originalUrl(req)
+
+  return {
+    props: {
+      lightningAddress: `${username}@${url.hostname}`,
+      lnurl: bech32.encode(
+        "lnurl",
+        bech32.toWords(
+          Buffer.from(
+            `${url.protocol}//${url.hostname}/.well-known/lnurlp/${username}`,
+            "utf8",
+          ),
+        ),
+        1500,
+      ),
+      webURL: `${url.protocol}//${url.hostname}/${username}`,
+    },
+  }
+}
+
+export default function ({ lightningAddress, lnurl, webURL }) {
   const router = useRouter()
-  const { username } = router.query
   const componentRef = useRef()
+  const [qrType, setQR] = useState("lnurl")
 
   return (
     <>
@@ -23,10 +46,10 @@ const PrintQR = () => {
               <Card className="text-center">
                 <Card.Body>
                   <Card.Text>
-                    <h1>Pay {username}</h1>
+                    <h1>Pay {lightningAddress}</h1>
                     <QRCode
                       ecLevel="H"
-                      value={`https://ln.bitcoinbeach.com/${username}`}
+                      value={qrType === "lnurl" ? lnurl : webURL}
                       size={800}
                       logoImage="/BBQRLogo.png"
                       logoWidth={250}
@@ -47,10 +70,10 @@ const PrintQR = () => {
             <Card className="text-center">
               <Card.Body>
                 <Card.Text>
-                  <h3>Pay {username}</h3>
+                  <h3>Pay {lightningAddress}</h3>
                   <QRCode
                     ecLevel="H"
-                    value={`https://ln.bitcoinbeach.com/${username}`}
+                    value={qrType === "lnurl" ? lnurl : webURL}
                     size={300}
                     logoImage="/BBQRLogo.png"
                     logoWidth={100}
@@ -65,7 +88,15 @@ const PrintQR = () => {
         </Row>
         <br />
       </Container>
-      <Row className="justify-content-md-center">
+      <Row className="justify-content-center">
+        <Button
+          style={{ marginRight: 10 }}
+          onClick={() => {
+            setQR(qrType === "web" ? "lnurl" : "web")
+          }}
+        >
+          Use {qrType === "web" ? "lnurl" : "web URL"}
+        </Button>
         <ReactToPrint
           trigger={() => <Button>Print QR Code</Button>}
           content={() => componentRef.current}
@@ -81,5 +112,3 @@ const PrintQR = () => {
     </>
   )
 }
-
-export default PrintQR
