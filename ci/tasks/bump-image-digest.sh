be8c5b4811ad8e09d1df912c95f1e3168710d92e
Sandipan Dey <sandipandey.12@gmail.com>
2022-02-10 13:32:25 +0530

    ci: adds source code releasing (#170)
    * ci: added vendir sync of common tasks
    
    * ci: use synced in script and add src release
    
    * chore: add prettier ignores for yamls
    
    * ci: make bump pr update appVersion

--- ci/tasks/bump-image-digest.sh
+++ ci/tasks/bump-image-digest.sh
@@ -4,11 +4,13 @@ set -eu
 
 export digest=$(cat ./edge-image/digest)
 export ref=$(cat ./repo/.git/short_ref)
+export app_version=$(cat version/version)
 
 pushd charts-repo
 
 yq -i e '.image.digest = strenv(digest)' ./charts/galoy-pay/values.yaml
 yq -i e '.image.git_ref = strenv(ref)' ./charts/galoy-pay/values.yaml
+yq -i e '.appVersion = strenv(app_version)' ./charts/galoy-pay/Chart.yaml
 
 if [[ -z $(git config --global user.email) ]]; then
   git config --global user.email "bot@galoy.io"
