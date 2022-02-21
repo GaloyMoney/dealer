02684b505998897759a6fb7d0475ef74c6c63b33
Sebastien Verreault <sebver@pm.me>
2022-02-21 04:42:31 +0000

    fix: sample client

--- src/servers/price/sample_client.ts
+++ src/servers/price/sample_client.ts
@@ -1,54 +1,68 @@
+import { baseLogger } from "../../services/logger"
+import { toSats, usd2cents } from "../../utils"
 import { DealerPriceService } from "./client_service"
 
 const priceService = DealerPriceService()
-const someUsd = 100 as UsdCents
-const someSats = 100000 as Satoshis
+const someUsd = usd2cents(100)
+const someSats = toSats(100000)
 const fewMins = (2 * 60) as Seconds
 export const run = async function () {
-  console.info(
+  baseLogger.info(
+    { someSats },
     `GetCentsFromSatsForImmediateBuy from service: ${await priceService.getCentsFromSatsForImmediateBuy(
       someSats,
     )}`,
   )
-  console.info(
+  baseLogger.info(
+    { someSats },
     `GetCentsFromSatsForImmediateSell from service: ${await priceService.getCentsFromSatsForImmediateSell(
       someSats,
     )}`,
   )
-  console.info(
+  baseLogger.info(
+    { someSats, fewMins },
     `GetCentsFromSatsForFutureBuy from service: ${await priceService.getCentsFromSatsForFutureBuy(
       someSats,
       fewMins,
     )}`,
   )
-  console.info(
+  baseLogger.info(
+    { someSats, fewMins },
     `GetCentsFromSatsForFutureSell from service: ${await priceService.getCentsFromSatsForFutureSell(
       someSats,
       fewMins,
     )}`,
   )
 
-  console.info(
+  baseLogger.info(
+    { someUsd },
     `getSatsFromCentsForImmediateBuy from service: ${await priceService.getSatsFromCentsForImmediateBuy(
       someUsd,
     )}`,
   )
-  console.info(
+  baseLogger.info(
+    { someUsd },
     `getSatsFromCentsForImmediateSell from service: ${await priceService.getSatsFromCentsForImmediateSell(
       someUsd,
     )}`,
   )
-  console.info(
+  baseLogger.info(
+    { someUsd, fewMins },
     `getSatsFromCentsForFutureBuy from service: ${await priceService.getSatsFromCentsForFutureBuy(
       someUsd,
       fewMins,
     )}`,
   )
-  console.info(
+  baseLogger.info(
+    { someUsd, fewMins },
     `getSatsFromCentsForFutureSell from service: ${await priceService.getSatsFromCentsForFutureSell(
       someUsd,
       fewMins,
     )}`,
+  )
+
+  baseLogger.info(
+    `getCentsPerSatsExchangeMidRate from service: ${await priceService.getCentsPerSatsExchangeMidRate()}`,
   )
 }
 
