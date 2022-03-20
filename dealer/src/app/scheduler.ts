b3aae8aeda65c99b017a4bec6a5925eb45d662b7
Sebastien Verreault <sebver@pm.me>
2022-03-20 03:23:28 +0000

    fix: split startup from scheduler

--- dealer/src/app/scheduler.ts
+++ dealer/src/app/scheduler.ts
@@ -1,40 +1,27 @@
 import { baseLogger } from "../services/logger"
 import cron from "node-cron"
 import { Dealer } from "../Dealer"
-import { exporter } from "../servers/exporter/exporter"
-import nodePgMigrate from "node-pg-migrate"
-import { RunnerOption } from "node-pg-migrate/dist/types"
-import { priceService } from "../servers/price"
+import { wrapAsyncToRunInSpan } from "../services/tracing"
 
 const logger = baseLogger.child({ module: "cron" })
 
-const options = {
-  migrationsTable: "pgmigrations",
-  dir: "dealer/migrations",
-  createSchema: true,
-  schema: "dealer",
-  direction: "up",
-  count: 100,
-  verbose: true,
-  databaseUrl: process.env["DATABASE_URL"],
-} as RunnerOption
-
-const scheduler = async () => {
-  cron.schedule("0,10,20,30,40,50 * * * *", async () => {
-    const dealer = new Dealer(logger)
+export async function scheduler() {
+  cron.schedule("0,5,10,15,20,25,30,35,40,45,50,55 * * * *", async () => {
     try {
-      logger.info("Starting dealer")
-      await dealer.updatePositionAndLeverage()
+      const dealer = new Dealer(logger)
+      logger.info("Starting Dealer via Scheduler")
+      const DealerUpdatePositionAndLeverageTask = () => dealer.updatePositionAndLeverage()
+      const wrappedTask = wrapAsyncToRunInSpan({
+        namespace: "app.scheduler",
+        fn: DealerUpdatePositionAndLeverageTask,
+      })
+      const result = await wrappedTask()
+      baseLogger.info(
+        { result },
+        `dealer.updatePositionAndLeverage() returned: ${result}`,
+      )
     } catch (error) {
-      baseLogger.warn(`Error in Dealer job: ${error}`)
+      baseLogger.warn(`Error in Scheduler job: ${error}`)
     }
   })
 }
-
-Promise.all([nodePgMigrate(options).catch((err) => logger.error(err))]).then(() =>
-  Promise.all([
-    exporter().catch((err) => logger.error(err)),
-    priceService().catch((err) => logger.error(err)),
-    scheduler().catch((err) => logger.error(err)),
-  ]),
-)
