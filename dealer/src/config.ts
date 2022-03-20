b9a29df619008debfdd17712cd4f80c46c609ae2
Sebastien Verreault <sebver@pm.me>
2022-03-20 03:31:54 +0000

    fix: change dealer tracing service name to diff.

--- dealer/src/config.ts
+++ dealer/src/config.ts
@@ -21,5 +21,5 @@ export const yamlConfig = _.merge(defaultConfig, customConfig)
 export const tracingConfig = {
   jaegerHost: process.env.JAEGER_HOST || "localhost",
   jaegerPort: parseInt(process.env.JAEGER_PORT || "6832", 10),
-  tracingServiceName: process.env.TRACING_SERVICE_NAME || "galoy-dev",
+  tracingServiceName: process.env.TRACING_SERVICE_NAME || "galoy-dev-dealer",
 }
