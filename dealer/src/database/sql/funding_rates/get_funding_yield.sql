66493d6369b59bd1c26f6f480a81f662ef10c4f4
Sebastien Verreault <sebver@pm.me>
2022-04-29 08:35:08 +0000

    fix: pg agg call need schema qualification

--- dealer/src/database/sql/funding_rates/get_funding_yield.sql
+++ dealer/src/database/sql/funding_rates/get_funding_yield.sql
@@ -1,5 +1,5 @@
 SELECT 
-    ROUND(COALESCE(MUL( COALESCE(1 + funding_rate, 1) ) - 1, 0), 15) as  funding_yield
+    ROUND(COALESCE(dealer.mul( COALESCE(1 + funding_rate, 1) ) - 1, 0), 15) as  funding_yield
 FROM dealer.funding_rates 
 WHERE funding_rate IS NOT NULL
 AND exchange_id = (SELECT id FROM dealer.exchanges where exchange_name = lower(${exchangeName}))
