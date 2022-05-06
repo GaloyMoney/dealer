55aaaf0f875dd346b6b7948be179fce0899991b4
Sebastien Verreault <sebver@pm.me>
2022-05-06 08:13:30 +0000

    fix: annualize funding yield

--- dealer/src/database/sql/funding_rates/get_funding_yield.sql
+++ dealer/src/database/sql/funding_rates/get_funding_yield.sql
@@ -1,7 +1,6 @@
 SELECT 
-    ROUND(COALESCE(dealer.mul( COALESCE(1 + funding_rate, 1) ) - 1, 0), 15) as  funding_yield
+    ((1. + ROUND(COALESCE(dealer.mul( COALESCE(1 + funding_rate, 1) ) - 1, 0), 15))^(365.25 / ${numberOfDays})) - 1. as funding_yield
 FROM dealer.funding_rates 
 WHERE funding_rate IS NOT NULL
 AND exchange_id = (SELECT id FROM dealer.exchanges where exchange_name = lower(${exchangeName}))
 AND timestamp >= current_timestamp - make_interval(days => ${numberOfDays});
-
