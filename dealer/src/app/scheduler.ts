e40ce9d8b5c970b9d43aa44cd14ebdb1ef79676f
Juan P Lopez <dolcalmi@gmail.com>
2022-07-11 18:51:23 -0500

    fix: set root for scheduler cron span

--- dealer/src/app/scheduler.ts
+++ dealer/src/app/scheduler.ts
@@ -8,23 +8,25 @@ import { ErrorLevel } from "../Result"
 const logger = baseLogger.child({ module: "cron" })
 
 export async function scheduler() {
-  cron.schedule("0,5,10,15,20,25,30,35,40,45,50,55 * * * *", async () => {
-    try {
-      const dealer = new Dealer(logger)
-      logger.info("Starting Dealer via Scheduler")
-      const DealerUpdatePositionAndLeverageTask = () => dealer.updatePositionAndLeverage()
-      const wrappedTask = wrapAsyncToRunInSpan({
-        namespace: "app.scheduler",
-        fn: DealerUpdatePositionAndLeverageTask,
-      })
-      const result = await wrappedTask()
-      baseLogger.info(
-        { result },
-        `dealer.updatePositionAndLeverage() returned: ${result}`,
-      )
-    } catch (error) {
-      recordExceptionInCurrentSpan({ error, level: ErrorLevel.Warn })
-      baseLogger.warn(`Error in Scheduler job: ${error}`)
-    }
-  })
+  cron.schedule(
+    "0,5,10,15,20,25,30,35,40,45,50,55 * * * *",
+    wrapAsyncToRunInSpan({
+      root: true,
+      namespace: "app.scheduler",
+      fnName: "run",
+      fn: async () => {
+        try {
+          logger.info("Starting Dealer via Scheduler")
+
+          const dealer = new Dealer(logger)
+          const result = await dealer.updatePositionAndLeverage()
+
+          baseLogger.info({ result }, `Update position and leverage result`)
+        } catch (error) {
+          recordExceptionInCurrentSpan({ error, level: ErrorLevel.Warn })
+          baseLogger.warn({ error }, "Error in Scheduler job")
+        }
+      },
+    }),
+  )
 }
