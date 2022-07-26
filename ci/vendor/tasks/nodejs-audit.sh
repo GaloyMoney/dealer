a58e38979df77b01bd377a3621f50026cedcdf9b
galoybot <86534088+galoybot@users.noreply.github.com>
2022-07-26 16:25:09 +0200

    ci(shared): bump vendored ci files (#250)

--- ci/vendor/tasks/nodejs-audit.sh
+++ ci/vendor/tasks/nodejs-audit.sh
@@ -8,10 +8,11 @@ LEVEL=${LEVEL:-high}
 pushd ${REPO_ROOT}
 
 yarn audit --level ${LEVEL}
+audit_return=$?
 
 # See https://classic.yarnpkg.com/lang/en/docs/cli/audit for explanation of exit codes
-if [[ ${LEVEL} == "critical" ]] && [[ $? -ge 16 ]]; then
+if [[ ${LEVEL} == "critical" ]] && [[ ${audit_return} -ge 16 ]]; then
   exit 1
-elif [[ $? -ge 8 ]]; then
+elif [[ ${audit_return} -ge 8 ]]; then
   exit 1
 fi
