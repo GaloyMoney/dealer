1be67e7b2d41b2c59d0bfa876efdc35987cd34a6
Theophilus <60826700+Extheoisah@users.noreply.github.com>
2022-09-15 12:36:40 +0100

    fix: remove i18n from next config (#280)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   rewrites() {
     return [
       { source: "/.well-known/lnurlp/:username", destination: "/api/lnurlp/:username" },
