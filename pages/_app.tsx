8201804d1c1c86318ecec33e3486b53d5f1774a1
Theophilus <60826700+Extheoisah@users.noreply.github.com>
2022-09-20 11:01:37 +0100

    fix: resolve issue with username validation on PWA install (#294)
    * fix: fix url for lightning nodes
    
    * feat: add link to POS app in homepage
    
    Co-authored-by: Theophilus Isah <theophilus@Theophiluss-MBP.lan>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,6 +24,7 @@ export default function Layout({
         <meta name="theme-color" content="#536FF2" />
         <meta name="apple-mobile-web-app-status-bar" content="#536FF2" />
         <link rel="apple-touch-icon" href="/BBW-QRLOGO.png" />
+        <link rel="icon" type="image/png" href="/favicon.ico.png" />
         <link rel="manifest" href="/manifest.json" id="manifest" />
 
         <script
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
-import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import React from "react"
 import Card from "react-bootstrap/Card"
-import ListGroup from "react-bootstrap/ListGroup"
+import Col from "react-bootstrap/Col"
+import Container from "react-bootstrap/Container"
 import Jumbotron from "react-bootstrap/Jumbotron"
+import ListGroup from "react-bootstrap/ListGroup"
+import Row from "react-bootstrap/Row"
+
 import { gql, useQuery } from "@apollo/client"
 
 import { GRAPHQL_URI } from "../lib/config"
+import { useRouter } from "next/router"
 
 const GET_NODE_STATS = gql`
   query nodeIds {
@@ -19,9 +22,22 @@ const GET_NODE_STATS = gql`
 function Home() {
   const nodeUrl =
     GRAPHQL_URI.indexOf("testnet") === -1
-      ? `https://1ml.com/node/`
-      : `https://1ml.com/testnet/node/`
+      ? `https://1ml.com/testnet/node/`
+      : `https://1ml.com/node/`
   const { loading, error, data } = useQuery(GET_NODE_STATS)
+  const router = useRouter()
+  const [username, setUsername] = React.useState<string>("")
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    router.push(
+      {
+        pathname: `/merchant/${username}`,
+      },
+      undefined,
+      { shallow: true },
+    )
+  }
 
   return (
     <Container>
@@ -57,6 +73,31 @@ function Home() {
                               Connect the Bitcoin Beach Lightning node
                             </a>
                           )}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                          <form
+                            className="username-form"
+                            autoComplete="off"
+                            onSubmit={(event: React.FormEvent<HTMLFormElement>) =>
+                              handleSubmit(event)
+                            }
+                          >
+                            <label htmlFor="username">
+                              To use the <strong>POS</strong> app, enter your bitcoin
+                              beach username
+                            </label>
+                            <input
+                              type="text"
+                              name="username"
+                              value={username}
+                              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                                setUsername(event.target.value)
+                              }
+                              placeholder="username"
+                              required
+                            />
+                            <button>Submit</button>
+                          </form>
                         </ListGroup.Item>
                       </ListGroup>
                     </Card.Body>
