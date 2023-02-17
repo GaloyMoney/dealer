9d51641be5d1a46d8569f509db0f077b6e0bc8b7
UncleSamtoshi <88598461+UncleSamtoshi@users.noreply.github.com>
2023-02-17 02:48:38 -0600

    feat: support nostr zap requests (#414)
    * feat: support nostr zap requests
    
    * chore: make time expiry default Galoy settings of 1 day
    
    ---------
    
    Co-authored-by: nicolasburtey <nicolasburtey@users.noreply.github.com>

--- pages/api/lnurlp/[username].ts
+++ pages/api/lnurlp/[username].ts
@@ -9,8 +9,9 @@ import {
   InMemoryCache,
 } from "@apollo/client"
 import type { NextApiRequest, NextApiResponse } from "next"
+import Redis from "ioredis"
 
-import { GRAPHQL_URI_INTERNAL } from "../../../lib/config"
+import { GRAPHQL_URI_INTERNAL, NOSTR_PUBKEY } from "../../../lib/config"
 
 const ipForwardingMiddleware = new ApolloLink((operation, forward) => {
   operation.setContext(({ headers = {} }) => ({
@@ -62,54 +63,162 @@ const LNURL_INVOICE = gql`
       }
       invoice {
         paymentRequest
+        paymentHash
       }
     }
   }
 `
 
 type CreateInvoiceOutput = {
-  paymentRequest?: string
-  error?: Error
+  invoice?: {
+    paymentRequest: string
+    paymentHash: string
+  }
+  errors?: {
+    message: string
+  }[]
 }
 
-async function createInvoice(
-  walletId: string,
-  amount: number,
-  metadata: string,
-): Promise<CreateInvoiceOutput> {
-  const descriptionHash = crypto.createHash("sha256").update(metadata).digest("hex")
+type CreateInvoiceParams = {
+  walletId: string
+  amount: number
+  descriptionHash: string
+}
 
+async function createInvoice(params: CreateInvoiceParams): Promise<CreateInvoiceOutput> {
   const {
     data: {
       mutationData: { errors, invoice },
     },
   } = await client.mutate({
     mutation: LNURL_INVOICE,
-    variables: {
-      walletId,
-      amount,
-      descriptionHash,
-    },
+    variables: params,
   })
-  if (errors && errors.length) {
-    throw new Error(`Failed to get invoice: ${errors[0].message}`)
+
+  return { errors, invoice }
+}
+
+const nostrEnabled = !!NOSTR_PUBKEY
+
+let redis: Redis | null = null
+
+if (nostrEnabled) {
+  const connectionObj = {
+    sentinelPassword: process.env.REDIS_PASSWORD,
+    sentinels: [
+      {
+        host: `${process.env.REDIS_0_DNS}`,
+        port: Number(process.env.REDIS_0_SENTINEL_PORT) || 26379,
+      },
+      {
+        host: `${process.env.REDIS_1_DNS}`,
+        port: Number(process.env.REDIS_1_SENTINEL_PORT) || 26379,
+      },
+      {
+        host: `${process.env.REDIS_2_DNS}`,
+        port: Number(process.env.REDIS_2_SENTINEL_PORT) || 26379,
+      },
+    ],
+    name: process.env.REDIS_MASTER_NAME ?? "mymaster",
+    password: process.env.REDIS_PASSWORD,
   }
-  return invoice
+
+  redis = new Redis(connectionObj)
+
+  redis.on("error", (err) => console.log({ err }, "Redis error"))
 }
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-  const { username, amount } = req.query
+  console.log(NOSTR_PUBKEY)
+
+  const { username, amount, nostr } = req.query
   const url = originalUrl(req)
-
-  console.log({ headers: req.headers }, "request to NextApiRequest")
-
   const accountUsername = username ? username.toString() : ""
-  const metadata = JSON.stringify([
-    ["text/plain", `Payment to ${username}`],
-    ["text/identifier", `${username}@${url.hostname}`],
-  ])
-  let walletId: string
 
+  const walletId = await getUserWalletId(accountUsername, req)
+  if (!walletId) {
+    return res.json({
+      status: "ERROR",
+      reason: `Couldn't find user '${username}'.`,
+    })
+  }
+
+  const metadata = JSON.stringify([
+    ["text/plain", `Payment to ${accountUsername}`],
+    ["text/identifier", `${accountUsername}@${url.hostname}`],
+  ])
+
+  // lnurl options call
+  if (!amount) {
+    return res.json({
+      callback: url.full,
+      minSendable: 1000,
+      maxSendable: 100000000000,
+      metadata: metadata,
+      tag: "payRequest",
+      ...(nostrEnabled
+        ? {
+            allowsNostr: true,
+            nostrPubkey: NOSTR_PUBKEY,
+          }
+        : {}),
+    })
+  }
+
+  // lnurl generate invoice
+  try {
+    if (Array.isArray(amount) || Array.isArray(nostr)) {
+      throw new Error("Invalid request")
+    }
+
+    const amountSats = Math.round(parseInt(amount, 10) / 1000)
+    if ((amountSats * 1000).toString() !== amount) {
+      return res.json({
+        status: "ERROR",
+        reason: "Millisatoshi amount is not supported, please send a value in full sats.",
+      })
+    }
+
+    let descriptionHash: string
+
+    if (nostrEnabled && nostr) {
+      descriptionHash = crypto.createHash("sha256").update(nostr).digest("hex")
+    } else {
+      descriptionHash = crypto.createHash("sha256").update(metadata).digest("hex")
+    }
+
+    const { invoice, errors } = await createInvoice({
+      walletId,
+      amount: amountSats,
+      descriptionHash,
+    })
+
+    if ((errors && errors.length) || !invoice) {
+      console.log("error getting invoice", errors)
+      return res.json({
+        status: "ERROR",
+        reason: `Failed to get invoice: ${errors ? errors[0].message : "unknown error"}`,
+      })
+    }
+
+    if (nostrEnabled && nostr && redis) {
+      redis.set(`nostrInvoice:${invoice.paymentHash}`, nostr, "EX", 1440)
+    }
+
+    return res.json({
+      pr: invoice.paymentRequest,
+      routes: [],
+    })
+  } catch (err: unknown) {
+    console.log("unexpected error getting invoice", err)
+    res.json({
+      status: "ERROR",
+      reason: err instanceof Error ? err.message : "unexpected error",
+    })
+  }
+}
+
+const getUserWalletId = async (accountUsername: string, req: NextApiRequest) => {
   try {
     const { data } = await client.query({
       query: ACCOUNT_DEFAULT_WALLET,
@@ -119,44 +228,9 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
         "x-forwarded-for": req.headers["x-forwarded-for"],
       },
     })
-    walletId = data?.accountDefaultWallet?.id ? data?.accountDefaultWallet?.id : ""
+    return data?.accountDefaultWallet?.id
   } catch (err) {
-    return res.json({
-      status: "ERROR",
-      reason: `Couldn't find user '${username}'.`,
-    })
-  }
-
-  if (amount) {
-    if (Array.isArray(amount)) {
-      throw new Error("Invalid request")
-    }
-    const amountSats = Math.round(parseInt(amount, 10) / 1000)
-    if ((amountSats * 1000).toString() !== amount) {
-      return res.json({
-        status: "ERROR",
-        reason: "Millisatoshi amount is not supported, please send a value in full sats.",
-      })
-    }
-    const { paymentRequest, error } = await createInvoice(walletId, amountSats, metadata)
-    if (error instanceof Error) {
-      return res.json({
-        status: "ERROR",
-        reason: error instanceof Error ? error.message : "unexpected error",
-      })
-    }
-    return res.json({
-      pr: paymentRequest,
-      routes: [],
-    })
-  } else {
-    // first call
-    res.json({
-      callback: url.full,
-      minSendable: 1000,
-      maxSendable: 100000000000,
-      metadata: metadata,
-      tag: "payRequest",
-    })
+    console.log("error getting user wallet id:", err)
+    return undefined
   }
 }
